e1991a4b2eee3d7c318418324e6ff57b20542cf4
King Chung Huang <kinghuang@mac.com>
2015-02-28 13:00:04 -0700

    Wrap default code in main function
    Probably has no practical difference in the context of this app, but
    helps me for comprehension.

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,14 @@
 -- This comment enforces unit-test coverage for this file:
 -- coverage: 0
 
-channel.answer()
-channel.say("This is an example application. Enter any number followed by the pound sign.")
-local digits = channel.gather()
-channel.say(digits)
-channel.hangup()
+-- Main entry point for the app
+function main()
+	channel.answer()
+	channel.say("This is an example application. Enter any number followed by the pound sign.")
+	local digits = channel.gather()
+	channel.say(digits)
+	channel.hangup()
+end
 
+-- Kick off the app
+main()
