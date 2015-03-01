79df6a94f93589c6ffbbbca320471f4b22cb72a4
King Chung Huang <kinghuang@mac.com>
2015-03-01 12:32:04 -0700

    Implement say_hours_of_operation
    Assemble and say the hours of operation to the caller, based on the
    operating_hours key in the Organization table, and any overrides in the
    menu stack.
    
    How the hours are assembled into text depends on the magnitude of
    variations in the operating hours, as determined by the existence of
    hours for specific days of the week overriding the default hours.

--- src/actions.lua
+++ src/actions.lua
@@ -4,6 +4,7 @@
 local datastore = require 'summit.datastore'
 local json      = require 'json'
 local recording = require 'summit.recording'
+local time      = require 'summit.time'
 
 
 function dial_number(options)
@@ -37,6 +38,77 @@ function record_voicemail(options)
 end
 
 function say_hours_of_operation(options)
+	-- Get the time zone and hours of operation from the organization table.
+	local org = datastore.get_table('Organization', 'map')
+	local operating_hours = org:get_row_by_key('operating_hours').data
+	local timezone = org:get_row_by_key('timezone').data
+
+	-- Determine default hours and today's hours
+	local now = time.now(timezone.default)
+	local now_weekday_name = time.weekday_name(now)
+	local default_hours = json:decode(operating_hours.default)
+	local today_hours = operating_hours[now_weekday_name] and json:decode(operating_hours[now_weekday_name]) or default_hours
+
+	-- Adjust the hours based on the menu stack (TBD).
+	-- Use the menu stack to determine adjustments on the overall
+	-- hours of operation. For example, support might have different
+	-- hours than the overall organization.
+
+	-- Determine how best to give the operating hours, based on how many exceptions
+	-- to the default hours there are, and whether they are on work days or weekend days.
+	local typical_workweek_days = {'monday', 'tuesday', 'wednesday', 'thursday', 'friday'}
+	local typical_weekend_days = {'saturday', 'sunday'}
+
+	local workweek_exceptions = {}
+	local weekend_exceptions = {}
+ 
+	for k, day in ipairs(typical_workweek_days) do
+		if operating_hours[day] ~= nil then table.insert(workweek_exceptions, day) end
+	end
+	for k, day in ipairs(typical_weekend_days) do
+		if operating_hours[day] ~= nil then table.insert(weekend_exceptions, day) end
+	end
+	
+	-- Assemble phrases that will be joined together to form the
+	-- hours of operation message.
+	local phrases = {}
+
+	-- If the number of exceptions for weekdays is less than 3, then say the
+	-- hours for all workdays, followed by up to 2 exceptions. Otherwise, say
+	-- the hours for each work day separately.
+	for k, opts in ipairs({
+		{workweek_exceptions, 3, typical_workweek_days},
+		{weekend_exceptions, 0, typical_weekend_days}
+	}) do
+		local exception_days, exception_limit, all_days = unpack(opts)
+
+		if #exception_days < exception_limit then
+			local exception_conjunctions = {'Except', 'And'}
+			table.insert(phrases, string.format('We are open monday to friday from %s to %s.', default_hours[1], default_hours[2]))
+			for k, day in ipairs(exception_days) do
+				local exception_hours = json:decode(operating_hours[day])
+				if #exception_hours == 2 then
+					table.insert(phrases, string.format('%s, %s from %s to %s.', exception_conjunctions[k], day, exception_hours[1], exception_hours[2]))
+				else
+					table.insert(phrases, string.format('%s, closed %s', exception_conjunctions[k], day))
+				end
+			end
+		else
+			table.insert(phrases, 'We are open')
+			for k, day in ipairs(all_days) do
+				if k == #all_days then table.insert(phrases, 'And,') end
+				local day_hours = operating_hours[day] and json:decode(operating_hours[day]) or default_hours
+				if #day_hours == 2 then
+					table.insert(phrases, string.format('%s from %s to %s.', day, day_hours[1], day_hours[2]))
+				else
+					table.insert(phrases, string.format('Closed %s.', day))
+				end
+			end
+		end
+	end
+
+	-- Say the complete hours of operation to the caller.
+	channel.say(table.concat(phrases, ' '))
 end
 
 function cant_answer_out_partying(options)
