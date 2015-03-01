7aedd3c54d651073408b163b1366afe7f061ee74
King Chung Huang <kinghuang@mac.com>
2015-03-01 10:23:31 -0700

    Move assertion to first_target before channel.answer
    Clean up comments due to moving greeting into an action. And, move
    first_target assertion before channel.answer.

--- src/main.lua
+++ src/main.lua
@@ -7,15 +7,14 @@ local target    = require 'target'
 
 -- Main entry point for the app
 function main()
-	-- Get the configuration table for thea app.
+	-- Get the first target from the configuration table.
+	-- The first target is typically a menu.
 	local config = datastore.get_table('IVR Configuration2', 'string')
-
-	-- Get the greeting and first target. The first target is typically a menu.
 	local first_target = config:get_row_by_key('first_target')
+	assert(first_target ~= nil, 'first_target is not specified')
 
 	channel.answer()
 	perform_target('action:say_greeting')
-	assert(first_target ~= nil, 'first_target is not specified')
 	perform_target(first_target.data)
 	perform_target('action:say_closing')
 	channel.hangup()
