44b310dd9af16c7414db256a66c9417ccc673026
King Chung Huang <kinghuang@mac.com>
2015-03-01 13:59:59 -0700

    Expand on comments in main
    Clarify that the first target doesn’t have to be the main menu. It
    could be something that leads to the main menu, or it could be an
    action and not a menu at all.
    
    Also, explicitly state the basic flow. The code is pretty self
    explanatory, though.

--- src/main.lua
+++ src/main.lua
@@ -8,11 +8,17 @@ local target    = require 'target'
 -- Main entry point for the app
 function main()
 	-- Get the first target from the configuration table.
-	-- The first target is typically a menu.
+	-- The first target is typically a menu, but could also be
+	-- a special message (eg: a product recall) that then links
+	-- to the main menu, or really anything else. It could also
+	-- be an action, and not a menu at all.
 	local config = datastore.get_table('IVR Configuration2', 'string')
 	local first_target = config:get_row_by_key('first_target')
 	assert(first_target ~= nil, 'first_target is not specified')
 
+	-- The main flow: answer the call, say the greeting,
+	-- perform the first target, say the closing, and
+	-- hang up.
 	channel.answer()
 	perform_target('action:say_greeting')
 	perform_target(first_target.data)
